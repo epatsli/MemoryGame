432f4b3acba1f5ce190d4e10313350626800f13b
epatsli <patrycja.a.sliwinska@capgemini.com>
2018-08-30 07:15:12 +0200

    I don't know what happend

--- controller.js
+++ controller.js
@@ -3,15 +3,27 @@ var controller = function () {
     var startGame = function () {
         var initialNumberOfPieces = view.getInitialNumberOfPieces();
 
+        view.giveNewParty();
         game.startGame({
             numberOfPieces: initialNumberOfPieces
         });
 
         view.renderPieces(game.getPieces());
+       // alert(customerNumberPieces.getAttribute("value"));
+    },
 
+    addPieces = function () {
+        view.addPieces();
+      //  view.showNumberOfPieces();
+    },
+
+    highlight = function () {
+        view.highlight(game.getPieces());
     };
 
     return {
-        'startGame': startGame
+        'startGame': startGame,
+        'addPieces': addPieces,
+        'highlight': highlight
     }
 }();
--- view.js
+++ view.js
@@ -1,11 +1,47 @@
 'use strict'
 var view = (function () {
-    var getInitialNumberOfPieces = function () {
+
+  var getInitialNumberOfPieces = function () {
         //donm
         return document.getElementById("initialNumberOfPieces").value;
+    },
+    numberOfPieces = getInitialNumberOfPieces(),
+        viewPieces= [],
+    addPieces  = function () {
+        return numberOfPieces++;
+    },
+
+    showNumberOfPieces = function () {
+        document.getElementById('numberOfPieces').textContent = numberOfPieces.toString();
+    },
+
+    renderPieces = function (pieces) {
+        var i, piece;
+        for (i = 0; i < pieces.length; i++) {
+            piece = document.createElement("div");
+            piece.classList.add('piece');
+            document.getElementById('pieces').appendChild(piece);
+            viewPieces.push(piece);
+        }
+    },
+
+    highlight = function (pieces) {
+        viewPieces[1].style.backgroundColor= "#0000FF";
+    },
+
+    giveNewParty = function () {
+        var pieces = document.getElementsByClassName('piece');
+        while (pieces.length > 0) {
+            pieces[0].parentNode.removeChild(pieces[0]);
+        }
     };
 
     return {
-        'getInitialNumberOfPieces': getInitialNumberOfPieces
+        'getInitialNumberOfPieces': getInitialNumberOfPieces,
+        'addPieces': addPieces,
+        'showNumberOfPieces': showNumberOfPieces,
+        'renderPieces': renderPieces,
+        'highlight': highlight,
+        'giveNewParty': giveNewParty
     }
 })();
